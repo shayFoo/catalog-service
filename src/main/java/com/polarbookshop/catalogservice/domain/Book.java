aefc6ca49cbadeff42b679c34b5dc2bd48875f1a
shyfoo <oasis1975sun@gmail.com>
2025-02-16 15:12:17 +0900

    バリデーション処理を追加

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,9 +1,23 @@
 package com.polarbookshop.catalogservice.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
 public record Book(
+        @NotBlank(message = "the book ISBN must be defined.")
+        @Pattern(
+                regexp = "^([0-9]{10}|[0-9]{13})$",
+                message = "The ISBN format must be valid (either 10 or 13 digits)"
+        )
         String isbn,
+        @NotBlank(message = "the book title must be defined.")
         String title,
+        @NotBlank(message = "the book author must be defined.")
         String author,
+        @NotNull(message = "the book price must be defined.")
+        @Positive(message = "the book price must be greater than zero.")
         Double price
 ) {
 }
--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -2,6 +2,7 @@ package com.polarbookshop.catalogservice.web;
 
 import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +27,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Book addBookToCatalog(@RequestBody Book book) {
+    public Book addBookToCatalog(@Valid @RequestBody Book book) {
         return bookService.addBookToCatalog(book);
     }
 
@@ -37,7 +38,7 @@ public class BookController {
     }
 
     @PutMapping("/{isbn}")
-    public Book editBookDetails(@PathVariable String isbn, @RequestBody Book book) {
+    public Book editBookDetails(@PathVariable String isbn, @Valid @RequestBody Book book) {
         return bookService.editBookDetails(isbn, book);
     }
 }
