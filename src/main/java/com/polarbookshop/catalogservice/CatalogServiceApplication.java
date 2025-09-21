e7a8fa612e7710cb8995a978ec99b86c1e933c58
shyFoo <oasis1975sun@gmail.com>
2025-09-21 13:47:45 +0900

    Update Java version to 21 in build configuration and application entry point

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CatalogServiceApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 
